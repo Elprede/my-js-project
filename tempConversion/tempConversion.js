1d40cabddbfe099833da5c4a62ed1e487ff6c400
Ian Wong <ianwong88@hotmail.com>
2020-11-01 17:00:39 +1100

    finished TempConve

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
 const ftoc = function() {
+  let f = arguments[0];
+  let final = (f - 32) * (5 / 9);
+
+  return parseFloat(final.toFixed(1));
 
 }
 
 const ctof = function() {
+  let c = arguments[0];
+  let final = (c * 9 / 5) + 32.0
 
+  return parseFloat(final.toFixed(1));
 }
 
 module.exports = {
