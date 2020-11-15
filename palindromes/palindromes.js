8d2c360ca7a9d3f9ac3e718b3669f2a68ab06cde
Ian Wong <ianwong88@hotmail.com>
2020-11-15 21:00:27 +1100

    finish palindrome exercise

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,4 +1,20 @@
-const palindromes = function() {
+const palindromes = function(word) {
+    word = word.replaceAll(" ", "");
+    word = word.replaceAll(", ", "");
+    word = word.replaceAll("!", "");
+    word = word.replaceAll(",", "");
+    word = word.replaceAll(".", "");
+    word = word.toUpperCase();
+    
+    for (let i = 0; i < Math.floor(word.length / 2); i++) {
+        if (word[i] != word[word.length - i - 1]) {
+            console.log(word);
+            return false;
+            
+        }
+    }
+    console.log(word);
+    return true;
 
 }
 
