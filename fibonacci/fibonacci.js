d4122e2a2a6a71830626a4c3aad812ed40a8f60d
Ian Wong <ianwong88@hotmail.com>
2020-11-16 00:10:37 +1100

    fibonacci done

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    let i;
+    let fib = [];
 
+    fib[0] = 0;
+    fib[1] = 1;
+
+    for (i = 2; i <= a; i++) {
+        fib[i] = fib[i - 2] + fib[i - 1];
+    }
+    return fib[a] === undefined ? "OPPS" : fib[a];  
+    
 }
 
 module.exports = fibonacci
