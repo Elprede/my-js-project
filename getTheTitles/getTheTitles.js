0721565388500f9139d5e634e9753fef134f1bcf
Ian Wong <ianwong88@hotmail.com>
2020-11-23 10:48:35 +1100

    getthetiles start

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let arr = [];
+    for (e in books) {
+        arr.push(e.title);
+    }
 }
 
 module.exports = getTheTitles;
