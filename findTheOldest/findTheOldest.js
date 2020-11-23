0e2f18f5e3c284df9591dfa23db660578cdf2dac
Ian Wong <ianwong88@hotmail.com>
2020-11-23 15:23:04 +1100

    finished FindTheOldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,36 @@
-let findTheOldest = function() {
+const people = [
+    {
+      name: 'Carly',
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: 'Ray',
+      yearOfBirth: 1962,
+      yearOfDeath: 2011
+    },
+    {
+      name: 'Jane',
+      yearOfBirth: 1912,
+      yearOfDeath: 1941
+    },
+  ]
 
+let findTheOldest = function(people) {
+    let oldest = 0;
+    let livedAge = 0;
+    const oldestYear = people.reduce(function(obj, item) {
+        livedAge = item.yearOfDeath === undefined ? new Date().getFullYear() - item.yearOfBirth : item.yearOfDeath - item.yearOfBirth;
+        if (livedAge >= oldest) {
+            obj = item;
+            oldest = livedAge;
+        }
+        return obj;
+    
+    }, {});
+    return oldestYear;
 }
+
+
 
 module.exports = findTheOldest
