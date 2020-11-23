6a65a6d0e2620f6dd715410949d7cf44ce555b90
Ian Wong <ianwong88@hotmail.com>
2020-11-23 12:52:28 +1100

    finished getTheTitles

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,10 +1,13 @@
 const getTheTitles = function(books) {
     let arr = [];
     let i = 0;
-    //test
-    for (e in books) {
-        arr.push(e.title);
+    for (let e in books) {
+        arr.push(books[e].title);
     }
+
+    return arr;
+
 }
+
 
 module.exports = getTheTitles;
